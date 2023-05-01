e05149fb80db0c0321fa1eacd12f28717318a9ea
ZvonkoZlo <zvonko.zlopasa@fsre.sum.ba>
2023-05-01 21:37:53 +0200

    ConnectionString

---
+++ Agrokultura/Migrations/20230501191442_fer.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Agrokultura.Migrations
+{
+    /// <inheritdoc />
+    public partial class fer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- Agrokultura/Models/AgroContext.cs
+++ Agrokultura/Models/AgroContext.cs
@@ -53,7 +53,7 @@ public partial class AgroContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=agro;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=True");
+        => optionsBuilder.UseSqlServer("Data Source=fantom.fer.hr,3000;Initial Catalog=PI-01;User Id=pi01;Password=G1.aajmz!;TrustServerCertificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
